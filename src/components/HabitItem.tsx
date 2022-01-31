88528232171eb23a77509f372e43218ce49df958
Weiyi <zhuwy99@gmail.com>
2022-01-31 11:03:29 -0500

    Make completed a state and update bgColor accordingly.

--- src/components/HabitItem.tsx
+++ src/components/HabitItem.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {View, Text} from 'react-native';
 import {useTailwind} from 'tailwind-rn';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
@@ -7,26 +7,32 @@ import {HabitOverviewProps} from '../utils/types';
 const HabitItem = ({name, completed, goal, timePeriod}: HabitOverviewProps) => {
   const tailwind = useTailwind();
   const [checked, setChecked] = useState(false);
+  const [completedState, setCompletedState] = useState(completed);
   const checkBoxIconName = checked
     ? 'checkbox-marked'
     : 'checkbox-blank-outline';
-  const bgColor = completed >= goal ? 'bg-hl-blue' : 'bg-neutral-200';
+  const bgColor = completedState >= goal ? 'bg-hl-blue' : 'bg-neutral-200';
 
   const handleCheckBoxCheck = () => {
     setChecked(!checked);
   };
 
+  useEffect(() => {
+    if (checked) setCompletedState(prev => prev + 1);
+    else setCompletedState(prev => prev - 1);
+  }, [checked]);
+
   return (
     <View
       style={tailwind(
         `px-3 py-2 ${bgColor} flex-row justify-between items-center mb-4`,
-      )}
-    >
+      )}>
       <View>
         <Text style={tailwind('text-2xl font-SemiBold')}>{name}</Text>
         <Text
-          style={tailwind('text-sm font-Light')}
-        >{`${completed}/${goal} x ${timePeriod}`}</Text>
+          style={tailwind(
+            'text-sm font-Light',
+          )}>{`${completedState}/${goal} x ${timePeriod}`}</Text>
       </View>
       <MaterialCommunityIcons
         onPress={handleCheckBoxCheck}
