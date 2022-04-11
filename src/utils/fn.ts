ed75340ede574221628cb8de0df3acbb41a76567
Weiyi <zhuwy99@gmail.com>
2022-04-11 13:47:51 -0400

    Mistake

--- src/utils/fn.ts
+++ src/utils/fn.ts
@@ -150,3 +150,18 @@ export const calcCompetitionTotal = (habits: HabitWithUid[]) => {
 
   return total;
 };
+
+export const DeleteCompetitionRequest = (
+  competitionRequests: CompetitionRequest[],
+  uid: string,
+  requestUserId: string,
+) => {
+  const newCompetitionRequests = [...competitionRequests];
+  const index = newCompetitionRequests.findIndex(e => e.uid === requestUserId);
+  if (index > -1) {
+    newCompetitionRequests.splice(index, 1);
+  }
+  firebase.firestore().collection('users').doc(uid).update({
+    competitionRequests: newCompetitionRequests,
+  });
+};
