f4f17cd0a6b6abb77af7da9d9e340725d2f15ec5
Stephen Feria <stephen.feria003@gmail.com>
2022-05-13 23:35:11 -0500

    renamed project

--- android/app/src/main/java/com/habitlink/MainActivity.java
+++ android/app/src/main/java/com/habitlink/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "HabitLink";
+    return "Hablink";
   }
 
   @Override
